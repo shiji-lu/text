ad86a927124e2428d2b894132b2704a0b9737359
shiji-lu <118508477+shiji-lu@users.noreply.github.com>
2022-12-18 14:08:22 +0800

    Add files via upload

---
+++ 天天酷跑/天天酷跑/tools.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<graphics.h>
+
+//返回距离上一次调用间隔的时间（单位：ms），第一次调用时返回0
+int getDelay();
+void putimagePNG(int picture_x, int picture_y, IMAGE* picture);
+
+//适用于y<0以及y>0的任何情况
+void putimagePNG2(int x, int y, IMAGE* picture);
+void putimagePNG2(int x, int y, int winWidth,IMAGE*picture);
