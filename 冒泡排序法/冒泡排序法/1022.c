fbba17fff6b82dfb88dcbf41fe1685f4cbd08dde
shiji-lu <118508477+shiji-lu@users.noreply.github.com>
2022-12-21 22:06:55 +0800

    Add files via upload

---
+++ 冒泡排序法/冒泡排序法/1022.c
@@ -0,0 +1,80 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//
+//冒泡排序法
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[5]={1,5,9,8,3};
+//	int i = 0;
+//	
+//	//int sz = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = 0; j <4- i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int t = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = t;
+//			}
+//		}
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d", arr[i]);
+//	}
+//
+//	return 0;
+//}
+//
+// 
+// 
+// 
+// 
+// 
+//选择排序法
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 0,25,46,29,23 };
+//	for (int i = 0; i < 4; i++)
+//	{
+//		for (int j = i + 1; j < 5; j++)
+//		{
+//			if (arr[i] > arr[j])
+//			{
+//				int t = arr[i];
+//				arr[i] = arr[j];
+//				arr[j] = t;
+//			}
+//		}
+//	}
+//	for (int i = 0; i < 5; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
+
+
+
+
+ 
+////指针
+////#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 1;
+//	printf("%d%d\n", a, b);
+//	int* p = &a;
+//	int* m = &b;
+//	*p = 1;
+//	*m = 0;
+//	printf("%d%d\n", a,b);
+//	return 0;
+//}
+
+//动态数组
