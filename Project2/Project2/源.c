23f6a77d6b3edf479f42ac879edd8c97e735b17f
shiji-lu <766993892@qq.com>
2023-03-10 22:59:13 +0800

    1

---
+++ Project2/Project2/源.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+int main()
+{
+
+	int a1 = 0;
+	char c1 = 0;
+	char c2 = 0;
+	int a2 = 0;
+	scanf("%d%c%d%c", &a1, &c1, &a2, &c2);
+	return 0;
+
+}
